19a0b6cde380432065bb06195c624d3540697b6a
Stephen Ringer <stephen@peerfit.com>
2019-05-01 15:18:41 -0400

    skip adding duplicates to chrome_app_list

--- dmenu_scripts/chrome_app.sh
+++ dmenu_scripts/chrome_app.sh
@@ -6,7 +6,7 @@ choice=$(cat $HOME/.chrome_app_list | dmenu -p "Launch Chrome App")
 
 if [ -n "$choice" ]; then # If it's not empty
     if curl --output /dev/null --silent --head --fail "$choice"; then
-        echo "$choice" >> $HOME/.chrome_app_list
+        grep -qxF "$choice" $HOME/.chrome_app_list || echo "$choice" >> $HOME/.chrome_app_list
         google-chrome-stable --app="$choice" &
     fi
 fi
